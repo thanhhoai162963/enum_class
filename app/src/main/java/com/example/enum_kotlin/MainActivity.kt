8ee616f8ce91dc5fa2ce619fdbb55e9d220b42a7
thanhhoai162963 <hoaithanh249@gmail.com>
2021-01-06 21:24:01 +0700

    123

---
+++ app/src/main/java/com/example/enum_kotlin/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.enum_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val nv = NhanVien.AGE
+        Log.d("bbb",nv.name)
+        Log.d("bbb",nv.Transfrom())
+        for (i in NhanVien.values()){
+            Log.d("bbb",i.toString())
+        }
+    }
+}
+enum class NhanVien(var value:Int=0){
+    AGE(10) {
+        override fun Transfrom(): String {
+            return "555"
+        }
+
+        override fun toString(): String {
+            return "123"
+        }
+    },
+    NAME {
+        override fun Transfrom(): String {
+            TODO("Not yet implemented")
+        }
+    },
+    JOB {
+        override fun Transfrom(): String {
+            TODO("Not yet implemented")
+        }
+    };
+    abstract fun Transfrom(): String
+}
